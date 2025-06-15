7f035338c10aa5b363149486645653f8e479adf2
Tejesh Anand <tjanand@usc.edu>
2025-06-15 14:39:06 -0700

    base change

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/book-reviews/', // This should match your GitHub Pages repository name
+  base: '/', // Changed to root since this is a GitHub Pages user site
 }) 
