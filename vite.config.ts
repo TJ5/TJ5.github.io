4e6b5eabf49e006043eb1e4e0f0503b9dc1e2fd5
Tejesh Anand <tjanand@usc.edu>
2025-06-15 17:49:05 -0700

    viteConfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Changed to root since this is a GitHub Pages user site
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 }) 
